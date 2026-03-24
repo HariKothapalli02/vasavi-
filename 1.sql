acd7743a07ef3c22f38780c41b6b876deea4594e
Hari Kothapalli <harikothapalli314@gmail.com>
2026-03-24 10:28:03 +0530

    Update admin and staff credentials with new emails and passwords

--- 1.sql
+++ 1.sql
@@ -19,26 +19,26 @@ ALTER TABLE users ADD COLUMN is_best_outgoing TINYINT DEFAULT 0 AFTER signature_
 
 -- 3. Pre-load 6 Panel Members
 INSERT INTO users (name, email, password, role) VALUES 
-('Panel Member 1', 'panel1@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel'),
-('Panel Member 2', 'panel2@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel'),
-('Panel Member 3', 'panel3@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel'),
-('Panel Member 4', 'panel4@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel'),
-('Panel Member 5', 'panel5@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel'),
-('Panel Member 6', 'panel6@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'panel');
+('Panel Member 1', 'panel1@srivasaviengg.ac.in', '$2y$10$JEeqqf10I0VCyiy956ZnEuzGEUfgA.xmCVv9HZ9plPvbylcVI3FHq', 'panel'),
+('Panel Member 2', 'panel2@srivasaviengg.ac.in', '$2y$10$lfu8ly//biRYuTCnlqhO3.85H54YFjpdCXENHiBu3z1TwFGMJBiOC', 'panel'),
+('Panel Member 3', 'panel3@srivasaviengg.ac.in', '$2y$10$PrBbp.oRmMveQBrm0lQ/xu7lG/DPfGCj.zJCBazoO2SARH.vfMn/a', 'panel'),
+('Panel Member 4', 'panel4@srivasaviengg.ac.in', '$2y$10$S0iw4r.sYZNCANZVXwLbE.RBVRlIdBLP1JmIB2DdAOdKff.jci7tm', 'panel'),
+('Panel Member 5', 'panel5@srivasaviengg.ac.in', '$2y$10$TKxmnpMzLWFqoW3NW1iYlOQi7h3.WcRnw0bne38HsZBXyzV1t44aK', 'panel'),
+('Panel Member 6', 'panel6@srivasaviengg.ac.in', '$2y$10$ZmhpRaEvChxhuZWV/ItRieVidQX/SFNftK6xryHt4g/TrEpJ3RxCK', 'panel');
 
 -- 4. Pre-load Super Admin
 INSERT INTO users (name, email, password, role, department) VALUES 
-('Super Admin', 'super_admin@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', NULL);
+('Super Admin', 'iqac@srivasaviengg.ac.in', '$2y$10$aXsCr2r72C5xcRSj.VzFsOs.zZNKrwlwQLISFYzuHmAMsPzn71gx2', 'admin', NULL);
 -- Note: Default password is 'password'
 
 -- 5. Pre-load HOD details for Departments
 INSERT INTO users (name, email, password, role, department) VALUES 
-('HOD CSE/CST', 'hod_cse@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'CSE,CST'),
-('HOD CAI/AIM', 'hod_cai@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'CAI,AIM'),
-('HOD ECE/ECT', 'hod_ece@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'ECE,ECT'),
-('HOD EEE', 'hod_eee@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'EEE'),
-('HOD ME', 'hod_me@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'ME'),
-('HOD CE', 'hod_ce@sves.org.in', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'admin', 'CE');
+('HOD CSE/CST', 'hod_cse@srivasaviengg.ac.in', '$2y$10$WWP.hgTyTLkWpZPF2rpUiukByPAVTK7pVcuBv.eJqXuBraxHvF9BW', 'admin', 'CSE,CST'),
+('HOD CAI/AIM', 'hod_cai@srivasaviengg.ac.in', '$2y$10$fPtldMvLZXxgvBrwO/RKtefNxWbcU1tIgagXWYz1ExKs8NKoGdW1q', 'admin', 'CAI,AIM'),
+('HOD ECE/ECT', 'hod_ece@srivasaviengg.ac.in', '$2y$10$xz/b5D9kb.FnWOp4H2hhvOQEdci3Ng37CC3heUBeIWpKkh5il2Snq', 'admin', 'ECE,ECT'),
+('HOD EEE', 'hod_eee@srivasaviengg.ac.in', '$2y$10$402Ibo0Qdbkq6gO1d6WIiOEzcgYcIKS1Gs4YRxqbrnSWCn9WBOXr2', 'admin', 'EEE'),
+('HOD ME', 'hod_me@srivasaviengg.ac.in', '$2y$10$30s3a0QwM05BBUrWttRJVuQrXub0gE93aY8GTvm8xylKoHwJ03ZDC', 'admin', 'ME'),
+('HOD CE', 'hod_ce@srivasaviengg.ac.in', '$2y$10$aeGgEfEUbfifjYPneR9vN.NPpeYjX9kmyRwgTPqvijVeOR11honHm', 'admin', 'CE');
 
 -- 6. Unlock Students (Run this to restore access to students after import)
 UPDATE users SET is_submitted = 0 WHERE role = 'student';
